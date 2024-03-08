7b2572f8c148c9ac7fb8d1db3e30979184c413e8
Your Name <you@example.com>
2024-03-08 13:33:40 +0400

    добавили команду

---
+++ task1.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+Console.Write("Ввести число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ void PrintNumbersRecursive(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        PrintNumbersRecursive(m+1, n);
+    }
+}
+
+Console.Write($"Натуральные числа от " + m + " до " + n + ": ");
+Console.WriteLine();
+PrintNumbersRecursive(m, n);
